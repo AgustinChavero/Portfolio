cf5adad68b89db87f184e1637b0919adf443200c
AgustinChavero <agustin1996orellano@gmail.com>
2023-03-23 19:08:11 -0300

    Landing Terminada

--- src/Pages/Landing/Components/ImageProfile.jsx
+++ src/Pages/Landing/Components/ImageProfile.jsx
@@ -8,7 +8,7 @@ function ImageProfile() {
     <div>
       <Link to="/home">
         <motion.div
-          className="rounded-full"
+          className="rounded-full hover:opacity-100"
           animate={{
             boxShadow: [
               "20px 0px 30px rgb(239 68 68)",
@@ -20,7 +20,6 @@ function ImageProfile() {
               "20px 0px 30px rgb(239 68 68)",
             ],
             border: ["2px solid white"],
-            rotate: [1, -1, 1, -1, 1, -1, 0],
           }}
           transition={{
             duration: 3,
@@ -33,9 +32,16 @@ function ImageProfile() {
           <img
             src={Profile}
             alt="agustin_chavero"
-            className="w-96 rounded-full"
+            className="w-96 rounded-full hover:scale-90 transition-all"
+            title="Haga click aqui"
           />
         </motion.div>
+        {/* 
+        <div className="flex justify-center absolute mx-48">
+          <button className="bg-neutral-800 hover:bg-black hover:border-white text-white border-2 border-black absolute rounded-md p-1 transition-all">
+            Ingrese
+          </button>
+        </div> */}
       </Link>
     </div>
   );
